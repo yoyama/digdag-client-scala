cca481c1fd5f62d257287d04c03e59d7fa6f47e3
You Yamagata <youy@bg8.so-net.ne.jp>
2020-12-06 16:17:10 +0900

    Change script embeded file name to '.jar', refactoring.

--- bin/gen_shell.sh
+++ bin/gen_shell.sh
@@ -4,5 +4,5 @@
 # Script file name must be .jar or .zip Caused by Scala REPL limitation
 #
 echo "gen_shell.sh called"
-cat digdag-shell/src/main/sh/selfrun.sh digdag-shell/target/scala-2.13/digdag-shell.jar > target/digdag-shell.jar
+cat digdag-shell/src/main/sh/selfrun.sh digdag-shell/target/scala-2.13/digdag-shell-assembly.jar > target/digdag-shell.jar
 chmod +x target/digdag-shell.jar
